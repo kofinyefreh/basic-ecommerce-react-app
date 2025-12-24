434b900bb95c2dc1a3975e2a3403728ae18a7190
Kofi Nyefreh <nyefrehkofi@gmail.com>
2025-12-24 09:49:16 -0800

    implement data fetching to the menu and additional routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />, // Called layout route in react router
+    errorElement: <Error />, // Errors from the children elements will bubble up
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "cart", element: <Cart /> },
+      { path: "order/new", element: <CreateOrder /> },
+      { path: "order/:orderId", element: <Order /> },
+    ],
   },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  { path: "cart", element: <Cart /> },
-  { path: "order/new", element: <CreateOrder /> },
-  { path: "order/:orderId", element: <Order /> },
 ]);
 
 function App() {
